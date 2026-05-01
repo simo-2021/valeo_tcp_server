b78e5353ec5afa896fea7131f91bc34962bcf586
simo-2021 <at.simo@pm.me>
2026-05-01 23:36:10 +0200

    update 23h36

--- Makefile
+++ Makefile
@@ -1,22 +1,6 @@
-# 1. Définir le préfixe de cross-compilation (si lancé hors Buildroot)
-# Vous devez avoir installé : sudo apt install gcc-aarch64-linux-gnu
-CROSS_COMPILE ?= aarch64-linux-gnu-
+# Utilise le compilateur ARM si CROSS_COMPILE est défini, sinon gcc
+CC = $(CROSS_COMPILE)gcc
+CFLAGS = -Wall -g
 
-# 2. Utiliser ?= pour permettre à Buildroot d'injecter son propre compilateur
-CC ?= $(CROSS_COMPILE)gcc
-CFLAGS ?= -Wall -g
-
-# 3. Nom de l'exécutable (doit correspondre à votre fichier .mk)
-TARGET = valeo_ivc_socket
-SRC = valeo_ivc_socket.c
-
-all: $(TARGET)
-
-$(TARGET): $(SRC)
-	$(CC) $(CFLAGS) -o $(TARGET) $(SRC)
-
-clean:
-	rm -f $(TARGET)
-
-distclean: clean
-	rm -f *.o
+valeo_ivc_socket: valeo_ivc_socket.c
+	$(CC) $(CFLAGS) -o valeo_ivc_socket valeo_ivc_socket.c
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,15 +10,13 @@ AESD_ASSIGNMENTS_SITE =  git@github.com:simo-2021/valeo_tcp_server.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	# On force un clean dans votre dépôt pour supprimer tout binaire x86 résiduel
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/ clean
-	# On compile avec le compilateur ARM (TARGET_CC)
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/ all
+define VALEO_IVC_SOCKET_BUILD_CMDS
+    $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -o \
+    $(@D)/valeo_ivc_socket $(@D)/valeo_ivc_socket.c
 endef
 
-
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/valeo_ivc_socket $(TARGET_DIR)/usr/bin/
+define VALEO_IVC_SOCKET_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/valeo_ivc_socket \
+    $(TARGET_DIR)/usr/bin/valeo_ivc_socket
 endef
 
