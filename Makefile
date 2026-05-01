f7bc7a027579c94bf823d8c14da6a4818c9becb0
simo-2021 <at.simo@pm.me>
2026-05-01 21:25:52 +0200

    new make

--- Makefile
+++ Makefile
@@ -1,34 +1,19 @@
-# Définir un préfixe pour cross-compiler
-CROSS_COMPILE ?= #aarch64-linux-gnu-
+# Utilisez ?= pour permettre à Buildroot de fournir son compilateur
+CC ?= $(CROSS_COMPILE)gcc
+CFLAGS ?= -Wall -g
 
-# Nom de l'exécutable final
-TARGET = valeo_ivc_socket_cross_compiled
+# IMPORTANT : Le nom doit correspondre à ce que votre fichier .mk cherche
+TARGET = valeo_ivc_socket
 
-# Nom du fichier source
 SRC = valeo_ivc_socket.c
 
-# Compilateur utilisé
-CC = $(CROSS_COMPILE)gcc
-
-# Options de compilation :
-# -Wall  : active les warnings (recommandé pour débutants)
-# -g     : ajoute les infos de debug (utile pour gdb)
-CFLAGS = -Wall -g
-
-# Règle par défaut (celle exécutée si on tape juste "make")
 all: $(TARGET)
 
-# Règle pour créer l'exécutable
-# $@ = nom de la cible (valeo_ivc_socket)
-# $< = premier fichier dépendant (valeo_ivc_socket.c)
 $(TARGET): $(SRC)
-	#$(CC) $(CFLAGS) $< -o $@
 	$(CC) $(CFLAGS) -o $(TARGET) $(SRC)
 
-# Nettoyage : supprime l'exécutable
 clean:
 	rm -f $(TARGET)
 
-# Nettoyage total (ex: fichiers temporaires)
 distclean: clean
 	rm -f *.o
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,26 +5,19 @@
 # Works for builds root and qemu: 30DEC2025
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = e17d40f
+AESD_ASSIGNMENTS_VERSION = 79789cb
 AESD_ASSIGNMENTS_SITE =  git@github.com:simo-2021/valeo_tcp_server.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app  all
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server  all
 	
 	# 2. Compile votre serveur ECU à la RACINE du dépôt $(@D)
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/ valeo_ivc_socket
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/ all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# Créer le dossier de destination
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
 
-	# Utiliser $(@D)/conf/* si le dossier est à la racine de votre dépôt
-	# OU $(@D)/finder-app/conf/* s'il est dans finder-app
-	#$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 
 	# Installation du binaire
 	$(INSTALL) -m 0755 $(@D)/valeo_ivc_socket $(TARGET_DIR)/usr/bin/
