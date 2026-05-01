4a46bca2ae2d07adc01cb183fbdba05a32333464
simo-2021 <at.simo@pm.me>
2026-05-01 22:05:55 +0200

    Fix makefile and source

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-# Utilisez ?= pour permettre à Buildroot de fournir son compilateur
+# 1. Définir le préfixe de cross-compilation (si lancé hors Buildroot)
+# Vous devez avoir installé : sudo apt install gcc-aarch64-linux-gnu
+CROSS_COMPILE ?= aarch64-linux-gnu-
+
+# 2. Utiliser ?= pour permettre à Buildroot d'injecter son propre compilateur
 CC ?= $(CROSS_COMPILE)gcc
 CFLAGS ?= -Wall -g
 
-# IMPORTANT : Le nom doit correspondre à ce que votre fichier .mk cherche
+# 3. Nom de l'exécutable (doit correspondre à votre fichier .mk)
 TARGET = valeo_ivc_socket
-
 SRC = valeo_ivc_socket.c
 
 all: $(TARGET)
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,18 +5,18 @@
 # Works for builds root and qemu: 30DEC2025
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = f7bc7a0
+AESD_ASSIGNMENTS_VERSION = 7dcd433
 AESD_ASSIGNMENTS_SITE =  git@github.com:simo-2021/valeo_tcp_server.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	# On force CC et CROSS_COMPILE directement ici
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		CC="$(TARGET_CC)" \
-		CROSS_COMPILE="$(TARGET_CROSS)" \
-		-C $(@D)/ all
+	# On force un clean dans votre dépôt pour supprimer tout binaire x86 résiduel
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/ clean
+	# On compile avec le compilateur ARM (TARGET_CC)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/ all
 endef
+
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/valeo_ivc_socket $(TARGET_DIR)/usr/bin/
