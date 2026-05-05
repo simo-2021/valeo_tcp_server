168462288cd0d41e33c75a84ee273a52ee67f615
simo-2021 <at.simo@pm.me>
2026-05-05 11:06:00 +0200

    add files/folder recycle bin folder

--- rebuild_valeo_ecu.sh
+++ rebuild_valeo_ecu.sh
@@ -1,6 +1,7 @@
+
 cd buildroot/
 echo "--- Starting Clean ---"
-make aesd-assignments-dirclean
+make valeo-ecu-dirclean
 echo "--- Clean Done. Waiting 3 seconds... ---"
 sleep 3
 echo "--- Starting Build with $(nproc) cores ---"
