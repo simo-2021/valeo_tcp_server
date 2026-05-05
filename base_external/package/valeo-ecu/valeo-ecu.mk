bcd986afbf20b1b189a5a783af5e67fd26b1bc76
simo-2021 <at.simo@pm.me>
2026-05-05 11:01:58 +0200

    valeo_ecu package

--- base_external/package/valeo-ecu/valeo-ecu.mk
+++ base_external/package/valeo-ecu/valeo-ecu.mk
@@ -5,18 +5,18 @@
 # Works for builds root and qemu
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = d15fa4b
-AESD_ASSIGNMENTS_SITE =  git@github.com:simo-2021/valeo_tcp_server.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+VALEO_ECU_VERSION  = d15fa4b
+VALEO_ECU_SITE =  git@github.com:simo-2021/valeo_tcp_server.git
+VALEO_ECU_SITE_METHOD = git
+VALEO_ECU_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+define VALEO_ECU_BUILD_CMDS
     $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -o \
     $(@D)/valeo_ivc_socket $(@D)/valeo_ivc_socket.c
 endef
 
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/valeo_ivc_socket \
+define VALEO_ECU_INSTALL_TARGET_CMDS
+	    $(INSTALL) -D -m 0755 $(@D)/valeo_ivc_socket \
     $(TARGET_DIR)/usr/bin/valeo_ivc_socket
 endef
 
